fae3bd574499249af6f02ac72b8cef4d6117011f
wangbo0105 <820585630@qq.com>
2021-07-03 13:35:58 +0800

    Initial commit

---
+++ src/main/java/com/mfw/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.mfw.dao;
+
+import com.mfw.model.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
